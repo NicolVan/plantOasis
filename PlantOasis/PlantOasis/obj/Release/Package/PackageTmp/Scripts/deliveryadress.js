b93571fbd8916938abfbefa0f092de132195ec0b
Nikola Vaniščáková <161007729+NicolVan@users.noreply.github.com>
2024-12-16 13:22:50 +0100

    publish

---
+++ PlantOasis/PlantOasis/obj/Release/Package/PackageTmp/Scripts/deliveryadress.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const deliveryCheckbox = document.querySelector('.cb-delivery-address');
+    const deliveryAddressDiv = document.querySelector('.delivery-address');
+
+    if (deliveryCheckbox && deliveryAddressDiv) {
+        // Initially hide or show the delivery address fields based on checkbox state
+        deliveryAddressDiv.style.display = deliveryCheckbox.checked ? 'block' : 'none';
+
+        // Add event listener to toggle visibility
+        deliveryCheckbox.addEventListener('change', function () {
+            if (this.checked) {
+                // Show delivery address and remove 'hidden' class if it's present
+                deliveryAddressDiv.classList.remove('hidden');
+                deliveryAddressDiv.style.display = 'block';
+            } else {
+                // Hide delivery address
+                deliveryAddressDiv.style.display = 'none';
+            }
+        });
+    }
+});
---
+++ PlantOasis/Publish/Scripts/naplnspajzu.main.js
@@ -0,0 +1,93 @@
+﻿$(document).ready(function () {
+    // Contact
+    plantoasisContactFormApi();
+    // Menu bar
+    plantoasisMenuScrollTrigger();
+    // Cookies
+    if ($.cookie("plantoasis_cookies_ok") != '1') {
+        $('.cookies-div').show();
+    }
+});
+
+/* contact */
+function plantoasisContactFormApi() {
+    if ($('.api-password-group').length > 0) {
+        $.ajax('/Umbraco/PlantOasis/PlantOasisApi/ContactFormApiKey',
+            {
+                type: 'POST',
+                success: function (data) {
+                    $('.api-password-group #Password').val(data.MainKey);
+                    $('.api-password-group #ConfirmPassword').val(data.SubKey);
+                }
+            });
+    }
+}
+
+/* Menu bar */
+function plantoasisMenuScrollTrigger() {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.menu-bar').addClass('fixed');
+        }
+        if ($(this).scrollTop() < 10) {
+            $('.menu-bar').removeClass('fixed');
+        }
+    });
+}
+
+/* cookies */
+function plantoasisCookiesOk() {
+    $.cookie("plantoasis_cookies_ok", '1', { expires: 40000, path: '/' });
+    plantoasisCookiesClose();
+}
+function plantoasisCookiesClose() {
+    $('.cookies-div').hide();
+}
+
+/* slider */
+function plantoasisSlider() {
+    var slideCount = $('.slider ul li').length;
+    var slideWidth = $('.slider ul li').width();
+    var slideHeight = $('.slider ul li').height();
+    var sliderUlWidth = slideCount * slideWidth;
+
+
+    if (slideCount > 1) {
+        $('.slider').css({ width: slideWidth, height: slideHeight });
+        $('.slider ul li').css({ width: slideWidth });
+        $('.slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+        $('.slider ul li:last-child').prependTo('.slider ul');
+    }
+    else {
+        $('.slider a.control_prev').addClass('hidden');
+        $('.slider a.control_next').addClass('hidden');
+    }
+
+    function moveLeft() {
+        $('.slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('.slider ul li:last-child').prependTo('.slider ul');
+            $('.slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('.slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('.slider ul li:first-child').appendTo('.slider ul');
+            $('.slider ul').css('left', '');
+        });
+    };
+
+    $('.slider a.control_prev').click(function () {
+        moveLeft();
+        return false;
+    });
+
+    $('.slider a.control_next').click(function () {
+        moveRight();
+        return false;
+    });
+}
